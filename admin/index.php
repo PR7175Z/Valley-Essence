cd4e5d5894aad843b378fab41b1931b5d40e8e5b
PR7175Z <aadimpraz@gmail.com>
2024-09-14 18:59:56 +0545

    Added form

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,8 @@
 <?php include('theme-parts/header.php') ?>
 
+<form id="blogaddform" method="post">
+    <input type="text" name="blog_title" placeholder="Title">
+    <input type="submit" name="submit" value="Submit">
+</form>
+
 <?php include('theme-parts/footer.php') ?>
