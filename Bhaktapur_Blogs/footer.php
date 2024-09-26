6bd825e23c141c27aaca28df14237f7ee95fb757
Rohan Lakhemaru <rohanlakhemary167@gmail.com>
2024-09-26 08:18:04 +0545

    added banner section, blog-slider section, added js files and css files

--- Bhaktapur_Blogs/footer.php
+++ Bhaktapur_Blogs/footer.php
@@ -1,5 +1,7 @@
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js" integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/splide.min.js"></script>
     <script src="js/script.js"></script>
 </body>
 </html>
--- Bhaktapur_Blogs/header.php
+++ Bhaktapur_Blogs/header.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/splide.min.css">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
