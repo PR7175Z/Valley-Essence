fab0ed7eaca5120c7b59969e8ae879033bca4c54
PR7175Z <aadimpraz@gmail.com>
2024-11-27 08:25:02 +0545

    Added script

--- admin/theme-parts/footer.php
+++ admin/theme-parts/footer.php
@@ -1,2 +1,6 @@
+<script src="http://localhost/phpsite/blogsite/assets/js/jquery.min.js"></script>
+<script src="http://localhost/phpsite/blogsite/assets/js/bootstrap.js"></script>
+<script src="http://localhost/phpsite/blogsite/assets/js/imgblob.js"></script>
+<!-- <script src="http://localhost/phpsite/blogsite/assets/js/script.js"></script> -->
 </body>
 </html>
