4308bf0409ba27ed6c28b823209c3a1ee30d9e33
PR7175Z <aadimpraz@gmail.com>
2024-12-08 21:32:19 +0545

    Side navigation template

---
+++ admin/theme-parts/side-nav.php
@@ -0,0 +1,19 @@
+<div class="side-panel">
+    <ul>
+        <li>
+            <a href="#">Dashboard</a>
+        </li>
+        <li>
+            <a href="#">Add Blog</a>
+        </li>
+        <li>
+            <a href="#">Add Blog</a>
+        </li>
+        <li>
+            <a href="#">ADd Category</a>
+        </li>
+    </ul>
+    <div class="logout-btn">
+        <a href="logout.php" class="btn btn-secondary">logout</a>
+    </div>
+</div>
