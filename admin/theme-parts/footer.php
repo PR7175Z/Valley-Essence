dca7b6f06f3d74f78a453b933fed1099a2f8db35
PR7175Z <aadimpraz@gmail.com>
2024-12-08 21:31:28 +0545

    Closing tags

--- admin/theme-parts/footer.php
+++ admin/theme-parts/footer.php
@@ -1,6 +1,11 @@
-<script src="http://localhost/phpsite/blogsite/assets/js/jquery.min.js"></script>
-<script src="http://localhost/phpsite/blogsite/assets/js/bootstrap.js"></script>
-<script src="http://localhost/phpsite/blogsite/assets/js/imgblob.js"></script>
-<!-- <script src="http://localhost/phpsite/blogsite/assets/js/script.js"></script> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="http://localhost/phpsite/blogsite/assets/js/jquery.min.js"></script>
+    <script src="http://localhost/phpsite/blogsite/assets/js/bootstrap.js"></script>
+    <script src="http://localhost/phpsite/blogsite/assets/js/imgblob.js"></script>
+    <!-- <script src="http://localhost/phpsite/blogsite/assets/js/script.js"></script> -->
 </body>
 </html>
