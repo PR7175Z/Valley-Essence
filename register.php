3048b995a7ed53f25b69bf5d68057bf78d52f424
PR7175Z <aadimpraz@gmail.com>
2024-12-10 21:16:27 +0545

    Added userrole

--- register.php
+++ register.php
@@ -1,4 +1,42 @@
-<?php include('theme-parts/header.php') ?>
+<?php 
+    include('theme-parts/header.php');
+    //CHeck whether the Submit Button is Clicked or NOt
+    if(isset($_POST['submit']))
+    {
+        //Process for Login
+        //1. Get the Data from Login form
+        $userrole = 3;
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+        $user_email = $_POST['user_email'];
+        $username = $_POST['username'];
+        $password = md5($_POST['password']);
+
+        $sql = "SELECT email FROM userdata WHERE email = '$user_email'";
+        $duplicateEmail = mysqli_query($conn, $sql);
+        $sql = "SELECT username FROM userdata WHERE username = '$username'";
+        $duplicateusername = mysqli_query($conn, $sql);
+
+        if(mysqli_num_rows($duplicateEmail) > 0 || mysqli_num_rows($duplicateusername) > 0){?>
+            <div class='error'>Invalid user</div>
+        <?php }else{
+            $insert_query = "INSERT INTO `userdata`(`first_name`, `last_name`, `username`, `email`, `password`, `userrole`) VALUES ('$first_name', '$last_name', '$username','$user_email','$password', '$userrole')";
+            $result = mysqli_query($conn, $insert_query);
+
+            if( mysqli_affected_rows($conn)){
+                //User AVailable and Login Success
+                $_SESSION['login'] = "<div class='success'>Registration Successful.</div>";
+                $_SESSION['user'] = $username; //TO check whether the user is logged in or not and logout will unset it
+
+                //REdirect to HOme Page/Dashboard
+                header('location:'.SITEURL.'login.php/');
+                exit;
+            }else{
+                $_SESSION['login'] = "<div class='error'>Registration Failed.</div>";
+            }
+        }
+    }
+?>
 
 <section class="customer register-form-section section-gaps">
     <div class="container section-gaps">
@@ -40,42 +78,5 @@
 
     </div>
 </section>
-
-<?php 
-    //CHeck whether the Submit Button is Clicked or NOt
-    if(isset($_POST['submit']))
-    {
-        //Process for Login
-        //1. Get the Data from Login form
-        $first_name = $_POST['first_name'];
-        $last_name = $_POST['last_name'];
-        $user_email = $_POST['user_email'];
-        $username = $_POST['username'];
-        $password = md5($_POST['password']);
-
-        $sql = "SELECT email FROM userdata WHERE email = '$user_email'";
-        $duplicateEmail = mysqli_query($conn, $sql);
-        $sql = "SELECT username FROM userdata WHERE username = '$username'";
-        $duplicateusername = mysqli_query($conn, $sql);
-
-        if(mysqli_num_rows($duplicateEmail) > 0 || mysqli_num_rows($duplicateusername) > 0){?>
-<div class='error'>Invalid user</div>
-<?php }else{
-            $insert_query = "INSERT INTO `userdata`(`first_name`, `last_name`, `username`, `email`, `password`) VALUES ('$first_name', '$last_name', '$username','$user_email','$password')";
-            $result = mysqli_query($conn, $insert_query);
-
-            if( mysqli_affected_rows($conn)){
-                //User AVailable and Login Success
-                $_SESSION['login'] = "<div class='success'>Registration Successful.</div>";
-                $_SESSION['user'] = $username; //TO check whether the user is logged in or not and logout will unset it
-
-                //REdirect to HOme Page/Dashboard
-                header('location:'.SITEURL.'login.php/');
-            }else{
-                $_SESSION['login'] = "<div class='error'>Registration Failed.</div>";
-            }
-        }
-    }
-?>
 
 <?php include('theme-parts/footer.php') ?>
