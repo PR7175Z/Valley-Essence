9260b4d8c8e4446b1698b3024ef39cc00c435597
PR7175Z <aadimpraz@gmail.com>
2024-12-21 00:51:55 +0545

    ajax script

--- admin/user.php
+++ admin/user.php
@@ -151,31 +151,33 @@ if($users){
                                 <th>Name</th>
                                 <th>Email</th>
                                 <th>User Role</th>
+                                <th>Options</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td class="user-id">1</td>
-                                <td class="user-name">Admin</td>
-                                <td class="user-name">John Doe</td>
-                                <td class="user-email">johndoe@eample.com</td>
-                                <td class="user-role">Admin</td>
-                            </tr>
-                            <tr>
-                                <td class="user-id">1</td>
-                                <td class="user-name">Admin</td>
-                                <td class="user-name">John Doe</td>
-                                <td class="user-email">johndoe@eample.com</td>
-                                <td class="user-role">Admin</td>
-                            </tr>
-                            <tr>
-                                <td class="user-id">1</td>
-                                <td class="user-name">Admin</td>
-                                <td class="user-name">John Doe</td>
-                                <td class="user-email">johndoe@eample.com</td>
-                                <td class="user-role">Admin</td>
-                            </tr>
-
+                            <?php foreach($users as $user){
+                                if($user['status'] == 1){?>
+                                <tr>
+                                    <td class="user-id"><?php echo $user['id']; ?></td>
+                                    <td class="user-name"><?php echo $user['username']; ?></td>
+                                    <td class="user-name"><?php echo $user['first_name']; ?> <?php echo $user['last_name']; ?></td>
+                                    <td class="user-email"><?php echo $user['email']; ?></td>
+                                    <td class="user-role"><?php echo get_userrole($conn, $user['userrole'])[0]['Name']; ?></td>
+                                    <td>
+                                        <button class="approved status" data-action="approve" data-uid="<?php echo $user['id']; ?>">
+                                            <span class="icon">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
+                                            </span>
+                                        </button>
+                                        <button class="reject status" data-action="reject" data-uid="<?php echo $user['id']; ?>">
+                                            <span class="icon">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+                                            </span>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <?php }
+                            }?>
                         </tbody>
                     </table>
                 </div>
@@ -184,5 +186,30 @@ if($users){
     </div>
 
 </section>
+<script>
+document.querySelectorAll('.status').forEach(button => {
+    button.addEventListener('click', function () {
+        const action = this.dataset.action;
+        const userid = this.dataset.uid;
+
+        fetch('ajax-handler.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ action, userid }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                alert(`User role updated to ${action === 'approve' ? '1' : 'rejected'}`);
+            } else {
+                alert('Failed to update user role.');
+            }
+        })
+        .catch(error => console.error('Error:', error));
+    });
+});
+</script>
 <?php }
 include('theme-parts/footer.php') ?>
